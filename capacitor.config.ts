12a592acb0718807c89f6f2bef223c1e073597cc
VicenteObacoZh <gadpzamorach@gmail.com>
2026-05-04 15:08:33 -0500

    ajuste

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.soportecni.gpsrastreo',
+  appId: 'com.soportecni.gpsrastreo.app',
   appName: 'Soportecni GPS',
   webDir: 'www',
   server: {
